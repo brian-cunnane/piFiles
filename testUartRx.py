32f191ecfde80f12783baa410cf472b77aa33fd1
brian-cunnane <g00174469@gmit.ie>
2016-03-01 01:49:58 +0000

    received packet is now read into a list. Only output when array is correct size. Need to implement checksum on receiving side

--- testUartRx.py
+++ testUartRx.py
@@ -2,28 +2,23 @@
 #Author: Brian Cunnane
 #Date: 01/Feb/2016
 #Description: Script to read data on uart connection.
-#		Data to be deliniated by ";"
+#		Data is not deliniated
 import serial
 
 ser = serial.Serial("/dev/ttyAMA0")
 ser.baudrate = 9600
 data = ""
-row = 0
+myList = []
 print("Starting")
 while 1:
-#	print("starting")
 	try:
-		state = ser.read(1)
-#		if state.strip() != "0x0d":
-#			data += state
-#		else:
-#			print(data)
-#
-#			data = ""
-	#	print('z')
-		x = state.encode('hex')
-	#	x = ord(state)
-		print(x)
-	except:
+		for index in range(0,7):
+			state = ser.read(1)
+			x = state.encode('hex')
+			myList.append(x)
+			if len(myList) == 7:			
+				print myList
+				myList = []		
+	except IndexError:
 		print("Error")
 		pass
