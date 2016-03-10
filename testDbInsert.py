c496ecd6893b70c319f0d7bfcec379dc5f03631b
brian-cunnane <g00174469@gmit.ie>
2016-03-10 14:25:23 +0000

    Created python script to insert dumy values into database using mysql commands. Uses exception handling

---
+++ testDbInsert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#Author: Brian Cunnane
+#Description: Test script to insert values into database
+
+import MySQLdb
+import time
+
+now = time.strftime("%c")
+print "connecting"
+db = MySQLdb.connect("localhost","Administrator","admin","BEEKEEPER")
+cursor = db.cursor()
+
+try:
+	print "Inserting data"
+	cursor.execute("INSERT INTO HIVE1 (TEMPERATURE,HUMIDITY,WEIGHT,TIME,DATE) VALUES(24,90,150,CURTIME(),CURDATE())")
+	db.commit()
+	print "Data committed"
+
+except MySQLdb.Error as dbie:
+	print dbie
+	db.rollback
+
