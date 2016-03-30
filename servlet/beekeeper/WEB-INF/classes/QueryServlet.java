cf58136276c95b6e48622eb5331a9504455b90ce
brian-cunnane <g00174469@gmit.ie>
2016-03-30 20:55:02 +0100

    Test webpage to display data from db

--- servlet/beekeeper/WEB-INF/classes/QueryServlet.java
+++ servlet/beekeeper/WEB-INF/classes/QueryServlet.java
@@ -1,25 +1,28 @@
-
 import java.io.*;
 import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class QueryServlet extends HttpServlet{
+	Connection connection = null;
+	PreparedStatement find = null;
+	PrintWriter out = null;
         @Override
         public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
                 response.setContentType("text/html");
-                PrintWriter out = response.getWriter();
-                Connection connection = null;
-                Statement statement = null;
+                out = response.getWriter();
+                out.println("<html><head><title>Query Results</title></head><body>");
+                out.println("<h2>Thanks for your query.</h2>");
+		out.println("<p>Hello</p>");
                 try{
-                        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/BEEKEEPER","admin","Administrator");
-                        statement = connection.createStatement();
+			Class.forName("com.mysql.jdbc.Driver");
+                        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/BEEKEEPER","Administrator","admin");
                         String sql = "SELECT * FROM BEEKEEPER.HIVE1";
-                        out.println("<html><head><title>Query Results</title></head><body>");
-                        out.println("<h2>Thanks for your query.</h2>");
+                        
                         out.println("<p>Your Query is: " + sql + "</p>");
                         int count = 0;
-                        ResultSet rset = statement.executeQuery(sql);
+			find = connection.prepareStatement(sql);
+                        ResultSet rset = find.executeQuery();
                         while(rset.next()){
                                 out.println("<p>"+rset.getInt("Temperature")
                                                 +"," + rset.getInt("Weight")
@@ -30,7 +33,32 @@ public class QueryServlet extends HttpServlet{
                         }
                         out.println("<p>======" + count + " records found =====</p>");
                         out.println("</body></hmtl>");
-                }catch(SQLException SQLE){SQLE.printStackTrace();}
+                }catch(SQLException SQLE){
+			out.println("<p>SQL ERROR</p>");
+			SQLE.printStackTrace();
+			SQLE.printStackTrace(out);
+			out.println(stackTraceForWeb(SQLE));
+		}catch(ClassNotFoundException CNFE){
+			CNFE.printStackTrace();
+		}
+	}
+
+	public String stackTraceForWeb(Throwable t){
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		t.printStackTrace(pw);
+		String stackTrace = sw.toString();
+		pw.close();
+
+		return stackTrace.replace(System.getProperty("line.seperator"),"\n");
+	}
+
+	public void destroy(){
+		try{
+			find.close();
+			connection.close();
+			out.close();
+		}catch (SQLException E){E.printStackTrace();}
 	}
 }
 
