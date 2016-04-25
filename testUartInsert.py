4621db3444cf0f75955dd9a73b06907edd14ea11
brian-cunnane <g00174469@gmit.ie>
2016-04-25 22:18:00 +0100

    Updated script to accommodate extra byte in data packet. Added function to email warning to if fall is detected

--- testUartInsert.py
+++ testUartInsert.py
@@ -5,12 +5,17 @@
 #               Data is not deliniated
 import serial
 import MySQLdb
+import smtplib
+from email.mime.text import MIMEText
+import datetime
 
 ser = serial.Serial("/dev/ttyAMA0")
 ser.baudrate = 9600
 ser.flushInput()
 checksum = 0x00;
+weight = 0.0;
 myList = []
+emailSent = False
 print("Starting")
 while 1:
         try:
@@ -18,16 +23,27 @@ while 1:
 		cursor = db.cursor()
                 state = ser.read(1)
                 myList.append(state)
-                if len(myList) == 7:
-                        for index in range(2,6):
+                if len(myList) == 11:
+                        print myList
+                        for index in range(2,10):
                                 checksum = checksum ^ ord(myList[index])
-                        if checksum == ord(myList[6]):
-				for index in range(0,7):
+				print ("Checksum = %d") %checksum
+                        print checksum
+                        if checksum == ord(myList[10]):
+				for index in range(0,11):
 					myList[index] = ord(myList[index])
 				print myList
-				cursor.execute("INSERT INTO HIVE1(TEMPERATURE,WEIGHT,HUMIDITY,TIME,DATE) VALUES(%s,%s,%s,CURTIME(),CURDATE())",(myList[3],myList[4],myList[5]))
+				weight = (myList[4] << 24) + (myList[5] << 16) + (myList[6] << 8) + (myList[7])
+				weight = weight/1000.0
+				cursor.execute("INSERT INTO HIVE1(TEMPERATURE,WEIGHT,HUMIDITY,TIME,DATE) VALUES(%s,%s,%s,CURTIME(),CURDATE())",(myList[3],weight,myList[8]))
 				db.commit()
-			else: print ("Checksum mismatch, discarding packet")
+				if myList[9] > 50 or myList[9] < 0: #check Z axis value
+                                        if emailSent == False:
+                                                print("Warning; hive has fallen");
+                                                emailWarning()
+                                                emailSent = True
+                                else: emailSent = False;
+			else: print ("Checksum mismatch %d, discarding packet") %checksum
                         myList[:] = []
                         checksum = 0x00
         except (IndexError, serial.SerialException) as Error:
@@ -38,3 +54,27 @@ while 1:
 		print("Database Error")
 		print(DbError)
 
+        def emailWarning():
+                "Function to create a warning email if hive has fallen"
+                to='brian.cunnane@hotmail.com'
+                gmail_user = 'giantpizarro@gmail.com'
+                gmail_password = 'K5d;C2n?'
+                smtpserver = smtplib.SMTP('smtp.gmail.com',587)
+
+                smtpserver.ehlo() #identify self to extended smtp server
+                smtpserver.starttls()#start encryption
+                smtpserver.ehlo()#identify again as per documentation
+                smtpserver.login(gmail_user, gmail_password)
+
+                msg = MIMEText("Beekeeper has detected that a hive has fallen over \n")
+                msg['Subject'] = "URGENT fallen hive detected"
+                msg['From'] = gmail_user
+                msg['To'] = to
+
+                #send the message
+                smtpserver.sendmail(gmail_user, [to], msg.as_string())
+                #Close the smtp server
+                smtpserver.quit()
+
+                return;
+            
